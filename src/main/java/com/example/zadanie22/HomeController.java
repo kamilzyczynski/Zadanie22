977848d2949a52feef2f0e57cceff2742d220834
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-07-17 18:45:32 +0200

    Poprawione rozwiązanie

--- src/main/java/com/example/zadanie22/HomeController.java
+++ src/main/java/com/example/zadanie22/HomeController.java
@@ -1,7 +1,5 @@
 package com.example.zadanie22;
 
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
